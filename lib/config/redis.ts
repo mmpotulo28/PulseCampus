dabbfc7e17db337745a1872a8aaa8dcf9493a378
mmpotulo28 <mpotulom28@gmail.com>
2025-09-09 12:18:50 +0200

    refactor: update Redis configuration to use environment variables for enhanced security

--- lib/config/redis.ts
+++ lib/config/redis.ts
@@ -1,10 +1,10 @@
 import Redis from "ioredis";
 const redis = new Redis({
 	db: 0,
-	username: "default",
-	password: "yasAXIKPzNB96PNfzWhWYaA4zsJAn913",
-	host: "redis-15253.c73.us-east-1-2.ec2.redns.redis-cloud.com",
-	port: 15253,
+	username: process.env.REDIS_USERNAME || "",
+	password: process.env.REDIS_PASSWORD || "",
+	host: process.env.REDIS_HOST || "",
+	port: Number(process.env.REDIS_PORT) || 6379,
 }); // Defaults to localhost:6379
 
 export default redis;
