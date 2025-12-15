ea92a85d145865b71b4c1c75c5633b5152809ad1
mmpotulo28 <mpotulom28@gmail.com>
2025-12-15 08:52:21 +0200

    feat: add pnpm workspace configuration and update postcss and tailwind configs
    - Created a new pnpm workspace configuration file to specify built dependencies.
    - Updated postcss.config.js to use ES module syntax.
    - Added new icons for the application in the public/icons directory.
    - Created a manifest.json file for the web app with appropriate metadata and icons.
    - Added a service worker script (swe-worker) for caching strategies and handling requests.
    - Included source map for the service worker for easier debugging.
    - Refactored tailwind.config.js to use ES module syntax and simplified content paths.

--- next.config.js
+++ next.config.js
@@ -1,49 +1,58 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+import { withSentryConfig } from "@sentry/nextjs";
+
+const withPWA = withPWAInit({
+	dest: "public",
+	cacheOnFrontEndNav: true,
+	aggressiveFrontEndNavCaching: true,
+	reloadOnOnline: true,
+	swcMinify: true,
+	disable: process.env.NODE_ENV === "development",
+	workboxOptions: {
+		disableDevLogs: true,
+	},
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-      },
-    ],
-  },
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "randomuser.me",
+			},
+		],
+	},
 };
 
-module.exports = nextConfig;
+export default withSentryConfig(withPWA(nextConfig), {
+	// For all available options, see:
+	// https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-// Injected content via Sentry wizard below
+	org: "mpotulom",
+	project: "pulse-campus",
 
-const { withSentryConfig } = require("@sentry/nextjs");
+	// Only print logs for uploading source maps in CI
+	silent: !process.env.CI,
 
-module.exports = withSentryConfig(module.exports, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+	// For all available options, see:
+	// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-  org: "mpotulom",
-  project: "pulse-campus",
+	// Upload a larger set of source maps for prettier stack traces (increases build time)
+	widenClientFileUpload: true,
 
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
+	// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+	// This can increase your server load as well as your hosting bill.
+	// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+	// side errors will fail.
+	tunnelRoute: "/monitoring",
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+	// Automatically tree-shake Sentry logger statements to reduce bundle size
+	disableLogger: true,
 
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: "/monitoring",
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
+	// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+	// See the following for more information:
+	// https://docs.sentry.io/product/crons/
+	// https://vercel.com/docs/cron-jobs
+	automaticVercelMonitors: true,
 });
